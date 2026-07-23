52e54304ab9652870390fe276b7f7d24c58a5064
IsabellaKPM <ikpena@uc.cl>
2026-07-23 13:52:20 -0400

    feat: add ci and precommit config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,76 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import unusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
 
-const eslintConfig = defineConfig([
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = [
   ...nextVitals,
   ...nextTs,
-  globalIgnores([
-    ".next/**",
-    ".yarn/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    ignores: [
+      ".next/**",
+      ".yarn/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "commitlint.config.ts",
+      "eslint.config.mjs",
+    ],
+  },
+
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      "unused-imports": unusedImports,
+    },
+  },
+
+  {
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    rules: {
+      quotes: ["error", "double"],
+      "prettier/prettier": [
+        "error",
+        {
+          doubleQuote: true,
+          singleQuote: false,
+          trailingComma: "all",
+          endOfLine: "auto",
+        },
+      ],
+
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-argument": "error",
+      "@typescript-eslint/no-unsafe-member-access": "error",
+    },
+  },
+
   {
     files: ["types/**/*.ts", "types/**/*.d.ts"],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/ban-ts-comment": "off",
-      "@typescript-eslint/no-unused-vars": "off",
     },
   },
+
   eslintConfigPrettier,
-]);
+];
 
 export default eslintConfig;
